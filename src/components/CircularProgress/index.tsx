a671e663f2f17f5445298c572eb12f58f484b350
Dasha Taratynova <uenify@gmail.com>
2022-12-09 13:29:49 +0600

    feat: visual capacity

---
+++ src/components/CircularProgress/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+interface CircularProgressProps extends React.ComponentProps<'div'> {
+    size?: number;
+    color?: string;
+    thickness?: number;
+    value?: number;
+}
+
+export const CircularProgress: React.FC<CircularProgressProps> = ({
+    size = 40,
+    thickness = 3.6,
+    value = 0,
+    className,
+    ...props
+}) => {
+    const radius = React.useMemo(() => (size - thickness) / 2, [size, thickness]);
+    const circumference = React.useMemo(() => 2 * Math.PI * radius, [radius]);
+    const strokeDashoffset = React.useMemo(() => {
+        if (value < 100) {
+            return circumference - (value / 100) * circumference;
+        }
+        return 0;
+    }, [circumference, value]);
+
+    const color = React.useMemo(() => {
+        if (value < 100/3) {
+            return 'var(--c-red)';
+        }
+        if (value < 100/3*2) {
+            return 'var(--c-yellow)';
+        }
+        return 'var(--c-primary-600)';
+    }, [value]);
+
+    return (
+        <div
+            className={className}
+            style={{
+                width: size,
+                height: size,
+            }}
+            {...props}
+        >
+            <svg
+                viewBox={`0 0 ${size} ${size}`}
+                style={{
+                    transform: 'rotate(-90deg)',
+                }}
+            >
+                <circle
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    fill="transparent"
+                    stroke="var(--c-neutral-100)"
+                    strokeWidth={thickness}
+                />
+                <circle
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    fill="transparent"
+                    stroke={color}
+                    strokeWidth={thickness}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    strokeLinecap="round"
+                />
+            </svg>
+        </div>
+    );
+}
--- src/features/constructor/search/components/ScheduleItem/index.tsx
+++ src/features/constructor/search/components/ScheduleItem/index.tsx
@@ -1,6 +1,8 @@
 import { Card } from "@src/components/Card";
+import { CircularProgress } from "@src/components/CircularProgress";
 import { Property } from "@src/components/Property";
 import { Stack } from "@src/components/Stack";
+import { Tooltip } from "@src/components/Tooltip";
 import { Text, TextSkeleton } from "@src/components/Typography";
 import type { Schedule } from "@src/server/PCC/typings";
 import { typeDict, typeReg } from "@src/utils/data/getScheduleTypes";
@@ -23,7 +25,6 @@ export const ScheduleItemSkeleton: React.FC = () => {
                 </Stack>
                 <Stack direction="column" gap={4}>
                     <Property label="Instructor" value="Loading..." />
-                    <Property label="Capacity" value="Loading..." />
                     <Property label="Room" value="Loading..." />
                     <Property label="Time" value="Loading..." />
                 </Stack>
@@ -47,10 +48,15 @@ export const ScheduleItem: React.FC<{ schedule: Schedule }> = ({ schedule }) => 
                 </Stack>
                 <Stack direction="column" gap={4}>
                     <Property label="Instructor" value={schedule.faculty} />
-                    <Property label="Capacity" value={`${schedule.enr}/${schedule.capacity}`} />
                     <Property label="Room" value={schedule.room} />
                     <Property label="Time" value={schedule.times} />
                 </Stack>
+                <Tooltip delayDuration={0} content={`${schedule.enr}/${schedule.capacity}`}>
+                    <div className={cls.capacity}>
+                        <CircularProgress size={32} thickness={3} value={(Number(schedule.capacity) - schedule.enr) / Number(schedule.capacity) * 100} />
+                        <span className={cls.number}>{`${Number(schedule.capacity) - schedule.enr}`}</span>
+                    </div>
+                </Tooltip>
             </Stack>
         </Card>
     );
